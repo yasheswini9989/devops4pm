9389adaba4a235f99c5d3ec32916c2131d6d8611
yasheswini9989 <yasheswini41@gmail.com>
2026-07-31 10:23:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "one" {
   count         = 2
-  ami           = "ami-028ba4d4ccb4b7b72"
+  ami           = "ami-0e5497a77ef21b5ac"
   instance_type = "t3.micro"
 
   tags = {
