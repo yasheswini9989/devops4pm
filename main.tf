0bc2c07a1c7d79322311cc46ad67ef496b85d52b
yasheswini9989 <yasheswini41@gmail.com>
2026-08-03 10:29:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,19 +2,29 @@ provider "aws" {
   region = "us-east-2"
 }
 
-locals {
-  ingress_rules = [{ port = 443 }, { port = 80 }, { port = 22 }]
+# Get the default VPC
+data "aws_vpc" "default" {
+  default = true
 }
 
+locals {
+  ingress_rules = [
+    { port = 443 },
+    { port = 80 },
+    { port = 22 }
+  ]
+}
 
 resource "aws_security_group" "allow_tls" {
-  name        = "terra sg"
-  description = "Allow TLS inbound traffic"
+  name        = "terra-sg"
+  description = "Allow inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
 
   dynamic "ingress" {
     for_each = local.ingress_rules
+
     content {
-      description = "*"
+      description = "Allow TCP Port ${ingress.value.port}"
       from_port   = ingress.value.port
       to_port     = ingress.value.port
       protocol    = "tcp"
@@ -22,7 +32,15 @@ resource "aws_security_group" "allow_tls" {
     }
   }
 
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
-    Name = "terra sg"
+    Name = "terra-sg"
   }
 }
