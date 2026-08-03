8427598fd4983380975b2595fa23111bc84e4437
yasheswini9989 <yasheswini41@gmail.com>
2026-08-03 10:24:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,24 +2,27 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "one" {
-  count         = 2
-  ami           = "ami-0e5497a77ef21b5ac"
-  instance_type = "t3.micro"
+locals {
+  ingress_rules = [{ port = 443 }, { port = 80 }, { port = 22 }]
+}
 
-  user_data = <<-EOF
-#!/bin/bash
-sudo apt update -y
-sudo apt install apache2 git -y
-sudo systemctl enable apache2
-sudo systemctl start apache2
 
-cd /tmp
-git clone https://github.com/Ironhack-Archive/online-clone-amazon.git
-sudo cp -r online-clone-amazon/* /var/www/html/
-EOF
+resource "aws_security_group" "allow_tls" {
+  name        = "terra sg"
+  description = "Allow TLS inbound traffic"
+
+  dynamic "ingress" {
+    for_each = local.ingress_rules
+    content {
+      description = "*"
+      from_port   = ingress.value.port
+      to_port     = ingress.value.port
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
 
   tags = {
-    Name = "my-instance"
+    Name = "terra sg"
   }
 }
